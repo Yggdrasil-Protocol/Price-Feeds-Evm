46eb0756e3d8948a72ecb21d684279a1038cafd1
rudransh <rudransh9shinghal@gmail.com>
2024-06-19 22:28:14 +0530

    done

--- Price-Feeds/src/Counter.sol
+++ Price-Feeds/src/Counter.sol
@@ -1,6 +1,36 @@
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity ^0.8.13;
 
-contract PriceFeed {
-   
+import "../lib/openzeppelin-contracts/contracts/access/Ownable.sol";
+import "./IPriceFeed.sol";
+
+contract PriceFeed is Ownable, IPriceFeed  {
+    event PriceFeedUpdated();
+    event PriceFeedPublished();
+    event PriceFeedRequested();
+
+    struct Price { 
+        string pair ; 
+        uint256 price ; 
+        uint256 decimals ; 
+    }
+
+    struct Request {
+        string[] pair;
+        uint256[] decimals;
+    }
+
+    mapping (string pair => uint256 price ) Feed  ; 
+
+    function updatePriceFeed(Price calldata price ) public onlyOwner {
+
+    }
+
+    function publishPriceFeed(Price calldata price ) public onlyOwner {
+
+    }
+
+    function requesttPriceFeed(Request calldata request) public payable {
+    
+    }
 }
