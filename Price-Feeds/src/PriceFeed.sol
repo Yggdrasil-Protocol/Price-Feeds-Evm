c47222e374370ba6875b27ed9ef0b147cf2646b9
rudransh <rudransh9shinghal@gmail.com>
2024-06-29 00:28:41 +0530

    Contract : Natspec Added

--- Price-Feeds/src/PriceFeed.sol
+++ Price-Feeds/src/PriceFeed.sol
@@ -7,6 +7,10 @@ import "../lib/openzeppelin-contracts/contracts/access/Ownable.sol";
 import "../lib/openzeppelin-contracts/contracts/utils/math/SafeMath.sol";
 import "./IPriceFeedReciever.sol";
 
+/**
+ * @title PriceFeed
+ * @dev This contract allows for the updating and requesting of asset prices, managed by a trusted signer.
+ */
 contract PriceFeed is ReentrancyGuard, Ownable {
     // Function to receive Ether. msg.data must be empty
     receive() external payable {}
@@ -33,26 +37,47 @@ contract PriceFeed is ReentrancyGuard, Ownable {
     error InsufficientFee(uint256 required, uint256 provided);
     error TooManyAssets(uint256 provided, uint256 maximum);
     error PriceNotAvailable(bytes32 asset);
-
     error ZeroAddress();
     error TransferFailed();
 
+    /**
+     * @dev Initializes the contract with an initial fee per asset.
+     * @param _initialFeePerAsset The initial fee to be charged per asset request.
+     */
     constructor(uint256 _initialFeePerAsset) {
         trustedSigner = owner();
         feePerAsset = _initialFeePerAsset;
     }
 
+    /**
+     * @notice Sets a new trusted signer for price updates.
+     * @dev Only the owner can call this function.
+     * @param _newSigner The address of the new trusted signer.
+     */
     function setTrustedSigner(address _newSigner) external onlyOwner {
         if (_newSigner == address(0)) revert ZeroAddress();
         trustedSigner = _newSigner;
         emit TrustedSignerUpdated(_newSigner);
     }
 
+    /**
+     * @notice Sets a new fee per asset for price requests.
+     * @dev Only the owner can call this function.
+     * @param _newFeePerAsset The new fee to be charged per asset request.
+     */
     function setFeePerAsset(uint256 _newFeePerAsset) external onlyOwner {
         feePerAsset = _newFeePerAsset;
         emit FeeUpdated(_newFeePerAsset);
     }
 
+    /**
+     * @notice Updates the prices of multiple assets.
+     * @dev The function verifies the signature of the trusted signer.
+     * @param _assets The array of asset identifiers.
+     * @param _decimals The array of decimals for each asset price.
+     * @param _prices The array of prices for each asset.
+     * @param _signature The signature of the trusted signer.
+     */
     function updatePrice(
         bytes32[] calldata _assets,
         uint8[] calldata _decimals,
@@ -84,6 +109,12 @@ contract PriceFeed is ReentrancyGuard, Ownable {
         }
     }
 
+    /**
+     * @notice Requests the prices of multiple assets.
+     * @dev The function requires a fee to be paid and calls a callback function with the prices and decimals.
+     * @param _assets The array of asset identifiers.
+     * @param _callback The callback function to be called with the prices and decimals.
+     */
     function requestPrices(
         bytes32[] calldata _assets,
         function(uint256[] memory, uint8[] memory) external _callback
@@ -91,9 +122,12 @@ contract PriceFeed is ReentrancyGuard, Ownable {
         if (_assets.length > MAX_ASSETS_PER_REQUEST)
             revert TooManyAssets(_assets.length, MAX_ASSETS_PER_REQUEST);
 
-        // emit PricesRequested(msg.sender, _assets);
         uint8[] memory requestedDecimals = new uint8[](_assets.length);
         uint256[] memory requestedPrices = new uint256[](_assets.length);
+        uint256 fees = feePerAsset.mul(_assets.length);
+
+        if (msg.value < fees)
+            revert TransferFailed();
 
         for (uint256 i = 0; i < _assets.length; ) {
             bytes32 asset = _assets[i];
@@ -109,18 +143,16 @@ contract PriceFeed is ReentrancyGuard, Ownable {
             }
         }
 
-        if (msg.value < feePerAsset.mul(_assets.length))
-            revert InsufficientFee(feePerAsset.mul(_assets.length), msg.value);
-
         _callback(requestedPrices, requestedDecimals);
     }
 
+    /**
+     * @notice Withdraws all Ether from the contract to the owner's address.
+     * @dev Only the owner can call this function.
+     */
     function withdraw() public onlyOwner {
-        // get the amount of Ether stored in this contract
         uint256 amount = address(this).balance;
-
-        // send all Ether to owner
         (bool success, ) = owner().call{value: amount}("");
-        require(success, "Failed to send Ether");
+        if (!success) revert TransferFailed();
     }
 }
--- Price-Feeds/test/PriceFeed.t.sol
+++ Price-Feeds/test/PriceFeed.t.sol
@@ -3,364 +3,168 @@ pragma solidity ^0.8.0;
 
 import "forge-std/Test.sol";
 import "../src/PriceFeed.sol";
-import "../src/IPriceFeedReciever.sol";
-import "forge-std/console.sol";
-import "../lib/openzeppelin-contracts/contracts/utils/math/SafeMath.sol";
-
-contract MockConsumer {
-    uint256[] public resprices;
-    PriceFeed public priceFeed;
-
-    function _saveFeeds(uint256[] calldata prices) external {
-        resprices = prices;
-    }
-
-    function requestPrices(
-        PriceFeed _priceFeed,
-        bytes32[] calldata assets
-    ) external {
-        priceFeed = _priceFeed;
-        _priceFeed.requestPrices(assets, this._saveFeeds);
-    }
-}
 
 contract PriceFeedTest is Test {
     PriceFeed public priceFeed;
+    address public owner;
     address public trustedSigner;
     uint256 public initialFeePerAsset;
-    using SafeMath for uint256;
 
     function setUp() public {
-        trustedSigner = vm.addr(0x1);
+        owner = address(this);
+        trustedSigner = makeAddr("trustedSigner");
         initialFeePerAsset = 0.01 ether;
-        priceFeed = new PriceFeed(trustedSigner, initialFeePerAsset);
+        
+        priceFeed = new PriceFeed(initialFeePerAsset);
+        priceFeed.setTrustedSigner(trustedSigner);
     }
 
-    function testConstructor() public {
+    function testInitialState() public {
+        assertEq(priceFeed.owner(), owner);
         assertEq(priceFeed.trustedSigner(), trustedSigner);
         assertEq(priceFeed.feePerAsset(), initialFeePerAsset);
     }
 
     function testSetTrustedSigner() public {
-        address newSigner = vm.addr(2);
-        vm.prank(priceFeed.owner());
+        address newSigner = makeAddr("newSigner");
         priceFeed.setTrustedSigner(newSigner);
         assertEq(priceFeed.trustedSigner(), newSigner);
     }
 
-    function testSetTrustedSignerZeroAddress() public {
-        vm.prank(priceFeed.owner());
+    function testSetTrustedSignerRevertOnZeroAddress() public {
         vm.expectRevert(PriceFeed.ZeroAddress.selector);
         priceFeed.setTrustedSigner(address(0));
     }
 
     function testSetFeePerAsset() public {
         uint256 newFee = 0.02 ether;
-        vm.prank(priceFeed.owner());
         priceFeed.setFeePerAsset(newFee);
         assertEq(priceFeed.feePerAsset(), newFee);
     }
 
     function testUpdatePrice() public {
-        bytes32[] memory assets = new bytes32[](100);
+        bytes32[] memory assets = new bytes32[](2);
         assets[0] = keccak256("ETH");
         assets[1] = keccak256("BTC");
-        assets[2] = keccak256("LINK");
-        assets[3] = keccak256("AAVE");
-        assets[4] = keccak256("UNI");
-        assets[5] = keccak256("YFI");
-        assets[6] = keccak256("SUSHI");
-        assets[7] = keccak256("COMP");
-        assets[8] = keccak256("MKR");
-        assets[9] = keccak256("SNX");
 
-        assets[10] = keccak256("ADA");
-        assets[11] = keccak256("DOT");
-        assets[12] = keccak256("SOL");
-        assets[13] = keccak256("MATIC");
-        assets[14] = keccak256("ATOM");
-        assets[15] = keccak256("FTM");
-        assets[16] = keccak256("AVAX");
-        assets[17] = keccak256("LUNA");
-        assets[18] = keccak256("ALGO");
-        assets[19] = keccak256("VET");
+        uint8[] memory decimals = new uint8[](2);
+        decimals[0] = 18;
+        decimals[1] = 8;
 
-        assets[20] = keccak256("THETA");
-        assets[21] = keccak256("XTZ");
-        assets[22] = keccak256("TRX");
-        assets[23] = keccak256("NEO");
-        assets[24] = keccak256("XMR");
-        assets[25] = keccak256("EOS");
-        assets[26] = keccak256("IOTA");
-        assets[27] = keccak256("KSM");
-        assets[28] = keccak256("ZEC");
-        assets[29] = keccak256("BCH");
+        uint256[] memory prices = new uint256[](2);
+        prices[0] = 2000 * 10**18; // $2000 for ETH
+        prices[1] = 30000 * 10**8; // $30000 for BTC
 
-        assets[30] = keccak256("LTC");
-        assets[31] = keccak256("ZIL");
-        assets[32] = keccak256("ENJ");
-        assets[33] = keccak256("MANA");
-        assets[34] = keccak256("GRT");
-        assets[35] = keccak256("BAL");
-        assets[36] = keccak256("CRV");
-        assets[37] = keccak256("1INCH");
-        assets[38] = keccak256("RUNE");
-        assets[39] = keccak256("BAT");
-
-        assets[40] = keccak256("ANKR");
-        assets[41] = keccak256("STORJ");
-        assets[42] = keccak256("UMA");
-        assets[43] = keccak256("BAND");
-        assets[44] = keccak256("REN");
-        assets[45] = keccak256("CVC");
-        assets[46] = keccak256("RSR");
-        assets[47] = keccak256("OCEAN");
-        assets[48] = keccak256("MLN");
-        assets[49] = keccak256("LRC");
-
-        assets[50] = keccak256("KNC");
-        assets[51] = keccak256("REP");
-        assets[52] = keccak256("REQ");
-        assets[53] = keccak256("STMX");
-        assets[54] = keccak256("UOS");
-        assets[55] = keccak256("GNO");
-        assets[56] = keccak256("PERP");
-        assets[57] = keccak256("SRM");
-        assets[58] = keccak256("DYDX");
-        assets[59] = keccak256("FTT");
-
-        assets[60] = keccak256("CAKE");
-        assets[61] = keccak256("XVS");
-        assets[62] = keccak256("BEL");
-        assets[63] = keccak256("MDX");
-        assets[64] = keccak256("BAKE");
-        assets[65] = keccak256("ALPHA");
-        assets[66] = keccak256("DF");
-        assets[67] = keccak256("HNT");
-        assets[68] = keccak256("CTK");
-        assets[69] = keccak256("DIA");
-
-        assets[70] = keccak256("ORN");
-        assets[71] = keccak256("LINA");
-        assets[72] = keccak256("SPI");
-        assets[73] = keccak256("UBT");
-        assets[74] = keccak256("WNXM");
-        assets[75] = keccak256("AKRO");
-        assets[76] = keccak256("BELT");
-        assets[77] = keccak256("DEGO");
-        assets[78] = keccak256("AUDIO");
-        assets[79] = keccak256("TRB");
-
-        assets[80] = keccak256("AKT");
-        assets[81] = keccak256("CHR");
-        assets[82] = keccak256("PHA");
-        assets[83] = keccak256("BNT");
-        assets[84] = keccak256("ELF");
-        assets[85] = keccak256("ANT");
-        assets[86] = keccak256("POLY");
-        assets[87] = keccak256("BOND");
-        assets[88] = keccak256("DODO");
-        assets[89] = keccak256("QNT");
-
-        assets[90] = keccak256("RLC");
-        assets[91] = keccak256("CFX");
-        assets[92] = keccak256("REEF");
-        assets[93] = keccak256("OXT");
-        assets[94] = keccak256("COTI");
-        assets[95] = keccak256("SKL");
-        assets[96] = keccak256("GHST");
-        assets[97] = keccak256("MIR");
-        assets[98] = keccak256("FEI");
-        assets[99] = keccak256("RGT");
-
-        uint256[] memory prices = new uint256[](100);
-        prices[0] = 2000 ether;
-        prices[1] = 50000 ether;
-        prices[2] = 30 ether;
-        prices[3] = 300 ether;
-        prices[4] = 20 ether;
-        prices[5] = 30000 ether;
-        prices[6] = 10 ether;
-        prices[7] = 300 ether;
-        prices[8] = 2000 ether;
-        prices[9] = 100 ether;
-
-        prices[10] = 1.5 ether;
-        prices[11] = 15 ether;
-        prices[12] = 40 ether;
-        prices[13] = 1.2 ether;
-        prices[14] = 10 ether;
-        prices[15] = 0.25 ether;
-        prices[16] = 25 ether;
-        prices[17] = 80 ether;
-        prices[18] = 0.9 ether;
-        prices[19] = 0.12 ether;
-
-        prices[20] = 6 ether;
-        prices[21] = 2.5 ether;
-        prices[22] = 0.07 ether;
-        prices[23] = 35 ether;
-        prices[24] = 200 ether;
-        prices[25] = 4 ether;
-        prices[26] = 1 ether;
-        prices[27] = 300 ether;
-        prices[28] = 100 ether;
-        prices[29] = 500 ether;
-
-        prices[30] = 150 ether;
-        prices[31] = 0.08 ether;
-        prices[32] = 1.5 ether;
-        prices[33] = 0.5 ether;
-        prices[34] = 0.75 ether;
-        prices[35] = 12 ether;
-        prices[36] = 2.5 ether;
-        prices[37] = 3 ether;
-        prices[38] = 8 ether;
-        prices[39] = 0.25 ether;
-
-        prices[40] = 0.1 ether;
-        prices[41] = 0.5 ether;
-        prices[42] = 10 ether;
-        prices[43] = 7 ether;
-        prices[44] = 0.5 ether;
-        prices[45] = 0.03 ether;
-        prices[46] = 0.02 ether;
-        prices[47] = 0.7 ether;
-        prices[48] = 60 ether;
-        prices[49] = 0.25 ether;
-
-        prices[50] = 1 ether;
-        prices[51] = 15 ether;
-        prices[52] = 0.5 ether;
-        prices[53] = 0.02 ether;
-        prices[54] = 0.3 ether;
-        prices[55] = 100 ether;
-        prices[56] = 3 ether;
-        prices[57] = 6 ether;
-        prices[58] = 12 ether;
-        prices[59] = 25 ether;
-
-        prices[60] = 2 ether;
-        prices[61] = 20 ether;
-        prices[62] = 1 ether;
-        prices[63] = 2 ether;
-        prices[64] = 1.5 ether;
-        prices[65] = 0.25 ether;
-        prices[66] = 0.3 ether;
-        prices[67] = 15 ether;
-        prices[68] = 3 ether;
-        prices[69] = 2 ether;
-
-        prices[70] = 5 ether;
-        prices[71] = 0.15 ether;
-        prices[72] = 50 ether;
-        prices[73] = 0.8 ether;
-        prices[74] = 50 ether;
-        prices[75] = 0.01 ether;
-        prices[76] = 0.3 ether;
-        prices[77] = 0.25 ether;
-        prices[78] = 1.5 ether;
-        prices[79] = 30 ether;
-
-        prices[80] = 2 ether;
-        prices[81] = 0.5 ether;
-        prices[82] = 1 ether;
-        prices[83] = 2.5 ether;
-        prices[84] = 0.5 ether;
-        prices[85] = 4 ether;
-        prices[86] = 0.03 ether;
-        prices[87] = 40 ether;
-        prices[88] = 1 ether;
-        prices[89] = 70 ether;
-
-        prices[90] = 3 ether;
-        prices[91] = 0.25 ether;
-        prices[92] = 0.02 ether;
-        prices[93] = 0.5 ether;
-        prices[94] = 0.05 ether;
-        prices[95] = 0.08 ether;
-        prices[96] = 0.9 ether;
-        prices[97] = 1 ether;
-        prices[98] = 1 ether;
-        prices[99] = 15 ether;
-        bytes32 messageHash = keccak256(abi.encodePacked(assets, prices));
-
-        // Create the message hash
-
-        // Create the Ethereum Signed Message hash
-        bytes32 ethSignedMessageHash = keccak256(
-            abi.encodePacked("\x19Ethereum Signed Message:\n32", messageHash)
-        );
-
-        // Sign the message
-        (uint8 v, bytes32 r, bytes32 s) = vm.sign(0x1, ethSignedMessageHash);
-
-        // Concatenate the signature components
+        bytes32 messageHash = keccak256(abi.encodePacked(assets, prices, decimals));
+        bytes32 ethSignedMessageHash = keccak256(abi.encodePacked("\x19Ethereum Signed Message:\n32", messageHash));
+        (uint8 v, bytes32 r, bytes32 s) = vm.sign(uint256(keccak256(abi.encodePacked("trustedSigner"))), ethSignedMessageHash);
         bytes memory signature = abi.encodePacked(r, s, v);
 
-        // Update the price
-        priceFeed.updatePrice(assets, prices, signature);
+        priceFeed.updatePrice(assets, decimals, prices, signature);
 
-        // Verify the price was updated
-        assertEq(priceFeed.prices(assets[0] = keccak256("AAVE")), prices[3]);
+        assertEq(priceFeed.prices(assets[0]), prices[0]);
+        assertEq(priceFeed.prices(assets[1]), prices[1]);
     }
 
-    // function testUpdatePriceInvalidSignature() public {
-    //     bytes32 asset = keccak256("ETH");
-    //     uint256 price = 2000 ether;
-    //     uint256 timestamp = block.timestamp;
+    function testUpdatePriceInvalidSignature() public {
+        bytes32[] memory assets = new bytes32[](1);
+        assets[0] = keccak256("ETH");
 
-    //     bytes32 messageHash = keccak256(
-    //         abi.encodePacked(asset, price, timestamp)
-    //     );
-    //     (uint8 v, bytes32 r, bytes32 s) = vm.sign(2, messageHash); // Wrong signer
-    //     bytes memory signature = abi.encodePacked(r, s, v);
+        uint8[] memory decimals = new uint8[](1);
+        decimals[0] = 18;
 
-    //     vm.expectRevert(PriceFeed.InvalidSignature.selector);
-    //     priceFeed.updatePrice(asset, price, signature);
-    // }
+        uint256[] memory prices = new uint256[](1);
+        prices[0] = 2000 * 10**18;
 
-    // function testRequestPrices() public {
-    //     setUp();
-    //     updatePriceHelper(keccak256("ETH"), 2000 ether);
-    //     updatePriceHelper(keccak256("BTC"), 50000 ether);
-    //     updatePriceHelper(keccak256("LINK"), 30 ether);
+        bytes memory invalidSignature = new bytes(65);
 
-    //     bytes32[] memory assets = new bytes32[](3);
-    //     assets[0] = keccak256("ETH");
-    //     assets[1] = keccak256("BTC");
-    //     assets[2] = keccak256("LINK");
+        vm.expectRevert(PriceFeed.InvalidSignature.selector);
+        priceFeed.updatePrice(assets, decimals, prices, invalidSignature);
+    }
 
-    //     MockConsumer consumer = new MockConsumer();
+    function testRequestPrices() public {
+        // First, update some prices
+        bytes32[] memory assets = new bytes32[](2);
+        assets[0] = keccak256("ETH");
+        assets[1] = keccak256("BTC");
 
-    //     consumer.requestPrices(priceFeed, assets);
+        uint8[] memory decimals = new uint8[](2);
+        decimals[0] = 18;
+        decimals[1] = 8;
 
-    //     assertEq(consumer.resprices(0), 2000 ether);
-    //     assertEq(consumer.resprices(1), 50000 ether);
-    //     assertEq(consumer.resprices(2), 30 ether);
+        uint256[] memory prices = new uint256[](2);
+        prices[0] = 2000 * 10**18;
+        prices[1] = 30000 * 10**8;
 
-    // }
+        bytes32 messageHash = keccak256(abi.encodePacked(assets, prices, decimals));
+        bytes32 ethSignedMessageHash = keccak256(abi.encodePacked("\x19Ethereum Signed Message:\n32", messageHash));
+        (uint8 v, bytes32 r, bytes32 s) = vm.sign(uint256(keccak256(abi.encodePacked("trustedSigner"))), ethSignedMessageHash);
+        bytes memory signature = abi.encodePacked(r, s, v);
+
+        priceFeed.updatePrice(assets, decimals, prices, signature);
+
+        // Now request prices
+        uint256 requiredFee = priceFeed.feePerAsset() * assets.length;
+        MockPriceFeedReceiver receiver = new MockPriceFeedReceiver();
+
+        vm.deal(address(this), requiredFee);
+        priceFeed.requestPrices{value: requiredFee}(assets, receiver.receivePrice);
+
+        assertEq(receiver.receivedPrices(0), prices[0]);
+        assertEq(receiver.receivedPrices(1), prices[1]);
+        assertEq(receiver.receivedDecimals(0), decimals[0]);
+        assertEq(receiver.receivedDecimals(1), decimals[1]);
+    }
+
+    function testRequestPricesInsufficientFee() public {
+        bytes32[] memory assets = new bytes32[](2);
+        assets[0] = keccak256("ETH");
+        assets[1] = keccak256("BTC");
+
+        uint256 requiredFee = priceFeed.feePerAsset() * assets.length;
+        MockPriceFeedReceiver receiver = new MockPriceFeedReceiver();
+
+        vm.expectRevert(PriceFeed.TransferFailed.selector);
+        priceFeed.requestPrices{value: requiredFee - 1}(assets, receiver.receivePrice);
+    }
+
+    function testRequestPricesTooManyAssets() public {
+        bytes32[] memory assets = new bytes32[](101); // MAX_ASSETS_PER_REQUEST is 100
+        for (uint i = 0; i < 101; i++) {
+            assets[i] = keccak256(abi.encodePacked(i));
+        }
+
+        MockPriceFeedReceiver receiver = new MockPriceFeedReceiver();
+
+        vm.expectRevert(abi.encodeWithSelector(PriceFeed.TooManyAssets.selector, 101, 100));
+        priceFeed.requestPrices(assets, receiver.receivePrice);
+    }
 
     function testWithdraw() public {
-        uint256 amount = 1 ether;
-        vm.deal(address(priceFeed), amount);
+        // Send some Ether to the contract
+        vm.deal(address(priceFeed), 1 ether);
 
-        uint256 initialBalance = priceFeed.owner().balance;
-        vm.prank(priceFeed.owner());
+        uint256 initialBalance = address(this).balance;
         priceFeed.withdraw();
-        uint256 finalBalance = priceFeed.owner().balance;
+        uint256 finalBalance = address(this).balance;
 
-        assertEq(finalBalance - initialBalance, amount);
+        assertEq(finalBalance - initialBalance, 1 ether);
         assertEq(address(priceFeed).balance, 0);
     }
 
-    // function updatePriceHelper(bytes32 asset, uint256 price) internal {
-    //     bytes32 messageHash = keccak256(abi.encodePacked(asset, price));
-    //     bytes32 ethSignedMessageHash = keccak256(
-    //         abi.encodePacked("\x19Ethereum Signed Message:\n32", messageHash)
-    //     );
-    //     (uint8 v, bytes32 r, bytes32 s) = vm.sign(0x1, ethSignedMessageHash);
-    //     bytes memory signature = abi.encodePacked(r, s, v);
-    //     priceFeed.updatePrice(asset, price, signature);
-    // }
+
+    // Needed to receive Ether when calling withdraw()
+    receive() external payable {}
 }
+
+
+    // Helper contract to test price feed callback
+    contract MockPriceFeedReceiver  {
+        uint256[] public receivedPrices;
+        uint8[] public receivedDecimals;
+
+        function receivePrice(uint256[] memory _prices, uint8[] memory _decimals) external override {
+            receivedPrices = _prices;
+            receivedDecimals = _decimals;
+        }
+    }
